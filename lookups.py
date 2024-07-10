a990b6cf77228fb5c67485a6c413c677c5f1e15e
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2024-07-10 12:54:00 -0700

    marginal speed improvements

--- lookups.py
+++ lookups.py
@@ -1,11 +1,19 @@
 import asyncio
 import itertools
-import json
 import os.path
 import time
 
+from aiofiles import open as aio_open
 import aiohttp
 import requests
+import msgspec
+
+class Model(msgspec.Struct):
+    """A new type describing a Model"""
+    modelType: str
+    path: str
+    numSpecies: int
+    numReactions: int
 
 
 def request_metadata(checksum):
@@ -28,34 +36,33 @@ def get_metadata():
     if checksum != loadck:
         return request_metadata(checksum)
     with open("metadata.json", "r") as f:
-        return json.load(f)
+        return msgspec.json.decode(f.read(), type=list[Model])
 
 
-async def get(url, session, resultant_dir):
+async def downloadAntString(url, session, resultant_dir):
     try:
         async with session.get(url=url) as response:
             resp = await response.text()
-            with open(resultant_dir + url.split("/")[-1], "w") as f:
-                f.write(resp)
+            async with aio_open(os.path.join(resultant_dir, url.split("/")[-1]), 'w') as f:
+                await f.write(resp)
     except Exception as e:
         print("Unable to get url {} due to {}.".format(url, e.__class__))
 
 
 async def lookup(data, resultant_dir, num_species=None, num_reactions=None, model_type=None, ):
-    now = time.time()
     paths = []
     for item in data:
-        if (num_species is None or item.get('numSpecies') == num_species) and \
-                (num_reactions is None or item.get('numReactions') == num_reactions) and \
-                (model_type is None or item.get('modelType') == model_type):
+        if (num_species is None or item.numSpecies == num_species) and \
+                (num_reactions is None or item.numReactions == num_reactions) and \
+                (model_type is None or item.modelType == model_type):
             paths.append(
-                "https://raw.githubusercontent.com/epshteinmatthew/OscillatorDatabase/master/" + item["path"])
+                "https://raw.githubusercontent.com/epshteinmatthew/OscillatorDatabase/master/" + item.path)
     if not os.path.isdir(resultant_dir):
         os.mkdir(resultant_dir)
     async with aiohttp.ClientSession() as session:
         ret = await asyncio.gather(*(
-            get(url, session,
-                resultant_dir) for url in paths))
+            downloadAntString(url, session,
+                              resultant_dir) for url in paths))
 
 
 def get_summary(data, asString = False):
@@ -65,9 +72,9 @@ def get_summary(data, asString = False):
         summary = {
             "total amount of models": len(data),
         }
-    reaction_nums = {item['numReactions'] for item in data}
-    species_nums = {item['numSpecies'] for item in data}
-    model_types = {item['modelType'] for item in data}
+    reaction_nums = {item.numReactions for item in data}
+    species_nums = {item.numSpecies for item in data}
+    model_types = {item.modelType for item in data}
 
     combinations = itertools.product(
         reaction_nums,
@@ -75,8 +82,8 @@ def get_summary(data, asString = False):
         model_types
     )
     for nr, ns, mt in combinations:
-        amnt = len([item['path'] for item in data if
-                    item["numSpecies"] == ns and item["numReactions"] == nr and item["modelType"] == mt])
+        amnt = len([item.path for item in data if
+                    item.numSpecies == ns and item.numReactions == nr and item.modelType == mt])
         if (amnt != 0):
             if (asString):
                 summary += f"\nAmount of {mt} models with {nr} reactions and {ns} species: " + amnt.__str__()
@@ -86,16 +93,16 @@ def get_summary(data, asString = False):
 
 
 def get_total_number_of_model_types(data):
-    return len({item["modelType"] for item in data})
+    return len({item.modelType for item in data})
 
 
 def get_number_of_models_with_attrib(data, num_species=None, num_reactions=None, model_type=None):
     count = 0
 
     for item in data:
-        if (num_species is None or item.get('numSpecies') == num_species) and \
-                (num_reactions is None or item.get('numReactions') == num_reactions) and \
-                (model_type is None or item.get('modelType') == model_type):
+        if (num_species is None or item.numSpecies == num_species) and \
+                (num_reactions is None or item.numReactions == num_reactions) and \
+                (model_type is None or item.modelType == model_type):
             count += 1
 
     return count
@@ -103,6 +110,7 @@ def get_number_of_models_with_attrib(data, num_species=None, num_reactions=None,
 
 start = time.time()
 metadata = get_metadata()
+print(time.time() - start)
 
 #example: print a summary of the dataset
 print(get_summary(metadata, asString=True))
@@ -112,7 +120,7 @@ print(get_summary(metadata, asString=True))
 print(get_number_of_models_with_attrib(metadata, num_species=3, model_type="oscillator"))
 
 #example: lookup models with 3 species and 3 reactions, and put them into the "osc123" directory
-asyncio.run(lookup(metadata, "osc123/", 3, 3, "oscillator"))
+asyncio.run(lookup(metadata, "osc123/", 3, 4, "oscillator"))
 end = time.time()
 print(end - start)
 
